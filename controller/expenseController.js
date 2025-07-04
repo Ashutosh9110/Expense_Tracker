4d19944362c76ea370c7f55212bbcd8ac7ae6ceb
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-07-04 16:11:57 +0530

    Making Expense App Production ready

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -1,29 +1,34 @@
 
 
-
+const {sequelize} = require("../utils/db-connection")
 const {expenses} = require("../models/expenseModel")
 const { users } = require("../models/userModel");
 
 
 const addExpense = async (req, res) => {
   
+  let t
   try {
+
+    t = await sequelize.transaction()
     const { description, expenseAmount, category } = req.body
     const userId = req.userId
 
-    const newExpense  = await expenses.create({
+    await expenses.create({
       description, expenseAmount, category, userId 
-    })
+    }, { transaction : t})
 
 
     await users.increment("totalExpenses", {
       by: expenseAmount,
-      where: { id: userId }
+      where: { id: userId },
+      transaction: t
     });
 
-
+    await t.commit()
     res.status(200).json({ msg: "Expense added successfully" })
   } catch (error) {
+    if (t) await t.rollback();
     res.status(500).json({ msg : "Unable to add expense", error: error.message})
   }
 }
@@ -42,11 +47,16 @@ const getExpense = async (req, res) => {
 
 
 const deleteExpense = async (req, res) => {
+  let t
   try {
+    t = await sequelize.transaction()
     const id = req.params.id
-    const expense = await expenses.destroy({ where : {id}})
+    const expense = await expenses.destroy({ where : {id},
+    transaction : t })
+    await t.commit()
     res.status(200).json({ msg : "Expense deleted"})
   } catch (error) {
+    if (t) await t.rollback()
     res.status(500).json({ msg : "Unable to delete expense", error: error.message})
   }
 }
