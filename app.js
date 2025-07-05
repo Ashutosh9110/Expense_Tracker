36a9c90d51df714c85da660d4a6fb2b22a3645ab
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-07-05 15:39:33 +0530

    Report Generation Frontend (Premium Feature)

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const {sequelize} = require("./utils/db-connection")
 app.use(cors()) 
 
 app.use(express.json())
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use("/users", userRouter)
 app.use("/expenses", expenseRouter)
@@ -24,6 +25,10 @@ app.use("/password", resetPasswordRouter)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"))
 })
+
+app.get("/frontend", (req, res) => {
+  res.sendFile(__dirname + "/public/frontend.html");
+});
 
 
 sequelize.sync().then(() => {
